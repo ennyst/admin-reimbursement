319c2701e50eb249af9e119e059f05b51eefcedc
ennist <37688275+ennyst@users.noreply.github.com>
2018-07-17 14:13:44 +0700

    udah coy

---
+++ src/js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController',['$scope', function($scope){
+    $scope.title='Belajar AngularJs';
+    $scope.book={
+      title: 'Belajar AngularJs Bersama Agung Setiawan',
+      author : 'Agung Setiawan',
+      price  : 80000
+    };
+  }]);
